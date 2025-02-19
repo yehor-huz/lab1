e3897034472de28587a98a2e9adcddc07cdd5a67
Yehor <yegorhuz@gmail.com>
2025-02-19 11:02:06 +0200

    init commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.lab1',
+  appName: 'lab1',
+  webDir: 'www'
+};
+
+export default config;
