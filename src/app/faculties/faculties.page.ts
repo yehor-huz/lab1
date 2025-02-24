8260fd7227cd329ab3a7c1cdf0603d73b4e9823d
Yehor <yegorhuz@gmail.com>
2025-02-24 20:12:40 +0200

    fixed bug with pages

--- src/app/faculties/faculties.page.ts
+++ src/app/faculties/faculties.page.ts
@@ -2,13 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { LabHeaderComponent } from '../lab-header/lab-header.component';
 
 @Component({
   selector: 'app-faculties',
   templateUrl: './faculties.page.html',
   styleUrls: ['./faculties.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, LabHeaderComponent]
 })
 export class FacultiesPage implements OnInit {
 
